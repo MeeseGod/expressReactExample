9987a4c505e014c3e118ea9314e7e2143c721815
JosephR <joseph_rundell@outlook.com>
2022-11-14 15:13:32 -0500

    Complete example

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Users from './components/User';
+
+export default function App(){
+  return(
+    <Users />
+  )
+}
---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const users = require("./routes/users");
+
+app.use("/api/users", users);
+
+app.get("/api", (req, res) => {
+    res.send("hello world");
+});
+
+app.listen(1234);
